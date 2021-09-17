e4f33e6c3e5d4b1b3ab4c109385dcdeb43057bf3
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-09-17 12:38:12 +0200

    Add jest test for get-by-id function
    The test check just the success case, it must be expanded to check even the fail cases

---
+++ __tests__/unit/handlers/get-by-id.test.js
@@ -0,0 +1,27 @@
+// Import all functions from get-by-id.js
+const lambda = require('../../../dist/handlers/get-by-id.js');
+
+// This includes all tests for getByIdHandler()
+describe('Test getByIdHandler', () => {
+    it('Should return the volume "Harry Potter e la Pietra Filosofale"', async () => {
+
+        const item = {
+            id: '9CJWTbd-RYoC'
+        }
+
+        const event = {
+            httpMethod: 'GET',
+            pathParameters: {
+                id: '9CJWTbd-RYoC'
+            }
+        }
+
+        // Invoke the handler
+        const result = await lambda.getByIdHandler(event);
+        const responseBody = JSON.parse(result.body);
+
+        // Compare the result with the expected result
+        expect(result.statusCode).toEqual(200);
+        expect(responseBody.data.id).toEqual(item.id);
+    });
+});
