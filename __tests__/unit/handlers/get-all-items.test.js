3f55ba927a123b65b33417e41bb464c623e14c98
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-09-17 12:38:29 +0200

    Add jest test for get-all function
    The test check just the success case, it must be expanded to check even the fail cases

---
+++ __tests__/unit/handlers/get-all-items.test.js
@@ -0,0 +1,24 @@
+// Import all functions from get-all-items.js 
+const lambda = require('../../../dist/handlers/get-all.js');
+// Import dynamodb from aws-sdk 
+
+// This includes all tests for getAllItemsHandler() 
+describe('Test getAllHandler', () => {
+    it('should return ids', async () => {
+        const items = ['9CJWTbd-RYoC','OyTengEACAAJ' ];
+
+        const event = {
+            httpMethod: 'GET',
+            queryStringParameters: {
+                q: 'Harry+Potter'
+            }
+        }
+ 
+        // Invoke helloFromLambdaHandler() 
+        const result = await lambda.getAllHandler(event);
+        // const responseBody = JSON.parse(result.body);
+
+        // Compare the result with the expected result
+        expect(result.statusCode).toEqual(200);
+    });
+}); 
