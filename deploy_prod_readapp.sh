3c5bc8feb9f54f95625506a066a2453f56f386c9
Lorenzo Croccolino <lorenzo.croccolino@studio.unibo.it>
2021-10-26 17:40:52 +0200

    Add deploy script for local

---
+++ deploy_prod_readapp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export STACK_NAME=readapp-backend-volumes
+
+# Remove build directories, if them exist
+rm -rf dist/
+rm -rf .aws-sam/
+
+# Compile TS files and generate "dist" directory
+TYPE_ERRORS="$(tsc --project ./tsconfig.json)"
+
+# Check if TS compile exit with errors
+if [[ -n $TYPE_ERRORS ]]; then
+  echo "ERROR: "
+  echo "$TYPE_ERRORS"
+  exit
+fi
+
+# Build the project following SAM template and generate ".aws-sam" directory
+sam build \
+  --profile readapp
+
+# Deploy the built project to AWS
+sam deploy \
+  --s3-bucket readapp-cloudformation-backend \
+  --s3-prefix volumes/production \
+  --stack-name $STACK_NAME \
+  --capabilities CAPABILITY_IAM \
+  --profile readapp
